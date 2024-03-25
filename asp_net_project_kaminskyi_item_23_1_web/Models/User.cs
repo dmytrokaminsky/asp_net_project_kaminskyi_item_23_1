4edf3e1fa7a77930580aaf324000689e4a5debdf
dmytrokaminsky <dimon0661087874@gmail.com>
2024-03-25 15:24:42 +0200

    Lab 1

---
+++ asp_net_project_kaminskyi_item_23_1_web/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace asp_net_project_kaminskyi_item_23_1_web.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Cuctom error message")]
+        [Length(1, 2, ErrorMessage ="Field {0} has wrong lenth, it has to bee between {1} and {2}.")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
