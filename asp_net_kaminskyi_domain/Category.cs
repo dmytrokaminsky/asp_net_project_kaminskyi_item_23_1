451ec5c390f95876e1d97eb6cb73c5f2a27a8c9c
dmytrokaminsky <dimon0661087874@gmail.com>
2024-03-27 11:32:58 +0200

    Reapply "Lab 1"
    This reverts commit d1c4ad29c2552b37e66f4f68652e67836185e3f2.

---
+++ asp_net_kaminskyi_domain/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace asp_net_project_kaminskyi_item_23_1_web.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ asp_net_kaminskyi_domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace asp_net_kaminskyi_domain
+{
+    public class Class1
+    {
+
+    }
+}
