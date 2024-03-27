08049b8512bb6ad7aa18807aa616011f75d34529
dmytrokaminsky <dimon0661087874@gmail.com>
2024-03-27 11:09:33 +0200

    Lab 1

--- asp_net_project_kaminskyi_item_23_1_web/Program.cs
+++ asp_net_project_kaminskyi_item_23_1_web/Program.cs
@@ -1,4 +1,7 @@
 
+using asp_net_kaminskyi_persistance;
+using Microsoft.EntityFrameworkCore;
+
 namespace asp_net_project_kaminskyi_item_23_1_web
 {
     public class Program
@@ -13,6 +16,9 @@ namespace asp_net_project_kaminskyi_item_23_1_web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ShopDatabaseContext>(options => options.UseSqlServer(
+                builder.Configuration.GetConnectionString("ShopDatabase")));
 
             var app = builder.Build();
 
